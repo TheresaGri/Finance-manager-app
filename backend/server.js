74802399be97981d651565a85fe37e1606f3063f
Theresa Grimus <T.Grimus@gmx.net>
2023-03-04 10:29:54 +0100

    added data

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {transactionsRouter} from "./routes/transactions.js";
+import { categoriesRouter } from "./routes/categories.js";
 import cors from "cors";
 const app = express();
 app.use(express.json());
@@ -9,5 +10,7 @@ app.use(cors());
 const PORT = 3000;
 
 app.use("/api/transactions", transactionsRouter);
+app.use("/api/categories", categoriesRouter);
+
 
 app.listen(PORT);
