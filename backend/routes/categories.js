b611f72e581fc1cc557bfee4fbea201e67bd24fc
AdrianMonemi <adrianmonemi6@gmail.com>
2023-03-04 14:38:20 +0100

    issue#17 first edit

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -7,14 +7,31 @@ categoriesRouter.get("/", (req,res) => {
   res.json(categoriesData);
 });
 
-categoriesRouter.get("/:categoryId", (req, res) => {
+// categoriesRouter.get("/:categoryId", (req, res) => {
+//   const categoriesData = JSON.parse(fs.readFileSync("./data/categories.json"));
+//   const category = categoriesData.find((category) => category.id === parseInt(req.params.categoryId));
+//   if (!category) {
+//     res.status(404).send("Catogory not found");
+//   } else {
+//     res.json(category);
+//   }
+// });
+
+categoriesRouter.patch("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
   const categoriesData = JSON.parse(fs.readFileSync("./data/categories.json"));
-  const category = categoriesData.find((category) => category.id === parseInt(req.params.categoryId));
-  if (!category) {
-    res.status(404).send("Catogory not found");
-  } else {
-    res.json(category);
+  const categoryIndex = categoriesData.findIndex((category) => category.id === id);
+
+  if (categoryIndex === -1) {
+    return res.status(404).send("Catogory not found");
   }
+
+  const updatedCategory = { ...categoriesData[categoryIndex], ...req.body };
+  categoriesData[categoryIndex] = updatedCategory;
+
+  fs.writeFileSync("./data/categories.json", JSON.stringify(categoriesData));
+
+  res.json(updatedCategory);
 });
 
 export {categoriesRouter};
