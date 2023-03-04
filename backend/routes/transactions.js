fdc5c7ac8577df56cb3d7384d29b59e449df477f
Theresa Grimus <T.Grimus@gmx.net>
2023-03-04 14:22:14 +0100

    .

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -36,4 +36,43 @@ transactionsRouter.get("/", (req, res) => {
   res.json(data);
 });
 
+transactionsRouter.post("/", (req, res) => {
+  const newTransaction = req.body;
+  let transactions = JSON.parse(fs.readFileSync("./data/transactions.json"));
+
+  let maxId = 0;
+  for (let transaction of transactions) {
+    maxId = Math.max(maxId, transaction.id);
+  }
+  newTransaction.id = maxId + 1;
+
+  transactions.push(newTransaction);
+  fs.writeFileSync("./data/transactions.json", JSON.stringify(transactions));
+  res.json({ status: "success" });
+});
+
+transactionsRouter.put("/:id", (req, res) => {
+  let id = Number(req.params.id);
+  const newTransaction = req.body;
+  const transactions = JSON.parse(fs.readFileSync("./data/events.json"));
+  const newTransactionIndex = transactions.findIndex(
+    (transaction) => transaction.id === id
+  );
+  const transactionId = transactions[newTransactionIndex].id;
+  transactions[newTransactionIndex] = { ...newTransaction, id: transactionId };
+  fs.writeFileSync("./data/transactions.json", JSON.stringify(transactions));
+});
+
+transactions.delete("/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const transactions = JSON.parse(fs.readFileSync("./data/transactions.json"));
+  const filteredTransactions = transactions.filter(
+    (transaction) => transaction.id !== id
+  );
+  fs.writeFileSync(
+    "./data/transactions.json",
+    JSON.stringify(filteredTransactions)
+  );
+});
+
 export { transactionsRouter };
