256dc44a2f332a3e90533783ba4616c674a758f9
AdrianMonemi <adrianmonemi6@gmail.com>
2023-03-04 12:47:28 +0100

    issue#5 first edition

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -10,8 +10,16 @@ transactionsRouter.get("/", (req, res) => {
   } else if (req.query.type === "income") {
     data = data.filter((transaction) => transaction.type === "Income");
   }
+
+  if (req.query.category) {
+    const categoryId = parseInt(req.query.category);
+    data = data.filter((transaction) => transaction.categoryId === categoryId);
+  }
+
   if (req.query.sortDescending === "date") {
     data.sort((a, b) => new Date(b.date) - new Date(a.date));
+  } else if (req.query.sortDescending === "amount") {
+    data.sort((a, b) => b.amount - a.amount);
   }
   res.json(data);
 });
--- frontend/src/components/Text.tsx
+++ frontend/src/components/Text.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 function Text(props: {text:string, className?:string}) {
     return <p className={props.className}>{props.text}</p>
 }
