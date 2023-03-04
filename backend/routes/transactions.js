b5944ce85ffa90c419e58ddd57fd913848c9c41f
Theresa Grimus <T.Grimus@gmx.net>
2023-03-04 12:54:13 +0100

    .

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -4,8 +4,7 @@ import fs from "fs";
 const transactionsRouter = express.Router();
 
 transactionsRouter.get("/", (req, res) => {
-  const data = JSON.parse(fs.readFileSync("./data/transactions.json"));
-  const categories = JSON.parse(fs.readFileSync("./data.categories.json"));
+  let data = JSON.parse(fs.readFileSync("./data/transactions.json"));
 
   if (req.query["sortAscending"] !== undefined) {
     if (req.query["sortAscending"] === "date") {
