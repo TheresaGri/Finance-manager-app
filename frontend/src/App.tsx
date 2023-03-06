d16c7471889e9132c223d6e26a775f12558e25ae
Manuel <manuel.vanek@gmail.com>
2023-03-06 08:58:49 +0100

    added logo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import TransactionList from '../src/features/transactionList/transactionList'
+import Image from './components/Image';
 
 function App() {
 
   return (
     <div className="App">
+      <Image src={"./src/assets/bank-logo.png"} className={"bankLogo"}/>
       <TransactionList />
     </div>
   )
---
+++ frontend/src/components/Image.tsx
@@ -0,0 +1,5 @@
+function Image(props: {src:string, className:string}) {
+    return <img src={props.src} className={props.className}/>
+}
+
+export default Image
