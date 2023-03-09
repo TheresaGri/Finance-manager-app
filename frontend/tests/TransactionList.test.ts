3ae212f89ec1544c2d2f05bafd95edacf85e025c
Manuel <manuel.vanek@gmail.com>
2023-03-09 17:03:53 +0100

    trying unit tests

---
+++ frontend/tests/TransactionList.test.ts
@@ -0,0 +1,11 @@
+import { describe, test } from '@jest/globals';
+import {render} from '@testing-library/react'
+import TransactionList from '../src/features/transactionList/TransactionList'
+test('test something', () => {
+    //Arrange
+    render(<TransactionList />);
+
+    //Act
+    
+
+})
